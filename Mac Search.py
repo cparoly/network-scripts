0702d85270a1f9ce5b03e7af2bb0f51c81084b27
vampep <vampep@optonline.net>
2020-08-20 14:20:13 -0400

    fixed code

--- Mac Search.py
+++ Mac Search.py
@@ -6,163 +6,191 @@ import re
 import ipaddress
 import getpass
 
-
 mac_regex = re.compile(r'[0-9a-f]{4}\.[0-9a-f]{4}\.[0-9a-f]{4}')
 int_regex = re.compile(r'Fa{1}\S*\d/\S*\d{1,2}|Gi{1}\S*\d/\S*\d|Eth{1}\d/\S*\d{1,2}|Te{1}\S*\d/\S*\d')
-int_po_regex = re.compile(r'Po{1}\d*')
+int_po_regex = re.compile(r'Po{1}\d+')
 int_regexes = [int_regex, int_po_regex]
-description_regex = re.compile(r'Description: (.*)', re.MULTILINE)
+description_regex = re.compile(r'nw-s\S*|ns-s\S*')
 access_vlan_regex = re.compile(r'switchport access vlan (\d*)', re.MULTILINE)
 
 user = input("Tacacs username: ")
-pwd = getpass.getpass("Tacacs password" )
-
+pwd = getpass.getpass("Tacacs password")
 
 try:
-	distribution = input("What is the distribution switch that will have the ARP table for these? ")
-	distribution = ipaddress.ip_address(distribution)
+    distribution = input("What is the distribution switch that will have the ARP table for these? ")
+    distribution = ipaddress.ip_address(distribution)
 except ValueError:
-	distribution = socket.gethostbyname(distribution)
+    distribution = socket.gethostbyname(distribution)
 
 while True:
-	try:
-		MAC = input("What is the MAC Address you are trying to find? (format xxxx.xxxx.xxxx): ")
-		if re.match("^[0-9a-f\.]{5}[0-9a-f\.]{5}[0-9a-f]{4}$", MAC.lower()) is not None:
-			break
-		else:
-			print('That is not the correct MAC ADDRESS format.')
-			continue
+    try:
+        MAC = input("What is the MAC Address you are trying to find? (format xxxx.xxxx.xxxx): ")
+        if re.match("^[0-9a-f\.]{5}[0-9a-f\.]{5}[0-9a-f]{4}$", MAC.lower()) is not None:
+            break
+        else:
+            print('That is not the correct MAC ADDRESS format.')
+            continue
 
-	except Exception('That is not the correct MAC ADDRESS format.'):
-		continue
+    except Exception('That is not the correct MAC ADDRESS format.'):
+        continue
 
 cisco = {
-	'device_type': 'cisco_ios',
-	'host': distribution,
-	'username': user,
-	'password': pwd
+    'device_type': 'cisco_ios',
+    'host': distribution,
+    'username': user,
+    'password': pwd
 }
 
 try:
-	dist_connect = ConnectHandler(**cisco)
+    dist_connect = ConnectHandler(**cisco)
 except NetMikoAuthenticationException:
-	print("Username/password incorrect")
+    print("Username/password incorrect")
 
 
 def ConnectToDevice(host):
-	print("Connecting to " + host)
-	switch = {
-		'device_type': 'cisco_ios',
-		'host': host,
-		'username': user,
-		'password': pwd
-	}
-	try:
-		net_connect = ConnectHandler(**switch)
-	except NetMikoAuthenticationException:
-		raise
+    print("Connecting to " + host)
+    switch = {
+        'device_type': 'cisco_ios',
+        'host': host,
+        'username': user,
+        'password': pwd
+    }
+    try:
+        net_connect = ConnectHandler(**switch)
+    except NetMikoAuthenticationException:
+        raise
 
-	except NetMikoTimeoutException:
-		raise
+    except NetMikoTimeoutException:
+        raise
 
-	return net_connect
+    return net_connect
+
 
 def GetNextSwitch(mac):
-	print('*' * 50)
-	print ('Getting next switch')
-	int = dist_connect.send_command("show mac address-table address " + mac, delay_factor=.1)
-	po = re.search(int_po_regex, int)
-	print(po)
-	if po is None:
-		po = re.search(int_regex, int)
-		if po is None:
-			return None
-		else:
-			po = po.group()
-	else:
-		po = po.group()
+    print('*' * 50)
+    print('Getting next switch')
+    int = dist_connect.send_command("show mac address-table address " + mac, delay_factor=.1)
+    print(int)
+    po = re.search(int_po_regex, int)
+    print(po)
+    if po is None:
+        po = re.search(int_regex, int)
+        if po is None:
+            return None
+        else:
+            po = po.group()
+    else:
+        po = po.group()
 
-	print(po)
-	interface = dist_connect.send_command("show run int " + str(po))
-	print(interface)
-	for description in interface.splitlines():
-		if 'description' in description.lower():
-			next_switch = description.split(' ')
+    print(po)
+    interface = dist_connect.send_command("show run int " + str(po))
+    print(interface)
+    for description in interface.splitlines():
+        if 'description' in description.lower():
+            next_switch = description.split(' ')
 
-	next_switch = [switch for switch in next_switch if 'ns-s' in switch]
-	next_switch = str(next_switch)[1:-1]
-	next_switch = next_switch.strip("'")
+    next_switch = [switch for switch in next_switch if 'ns-s' in switch]
+    next_switch = str(next_switch)[1:-1]
+    next_switch = next_switch.strip("'")
 
-
-
-	return next_switch
+    return next_switch
 
 
 def FindAttachedInterface(switch, mac):
-	try:
-		net_connect = ConnectToDevice(switch)
-	except NetMikoAuthenticationException:
-		return "Failed to Connect"
-	except NetMikoTimeoutException:
-		return "Failed to Connect"
-	interface = net_connect.send_command("show mac address-table address " + mac)
-	print(interface)
-	int = re.search(int_regex, interface)
+    try:
+        net_connect = ConnectToDevice(switch)
+    except NetMikoAuthenticationException:
+        return "Failed to Connect"
+    except NetMikoTimeoutException:
+        return "Failed to Connect"
+    interface = net_connect.send_command("show mac address-table address " + mac, delay_factor=.1)
+    print('*' * 50)
+    print(interface)
+    int = re.search(int_regex, interface)
+    print(int)
 
-	if int is None:
-		int = re.search(r'Po{1}\d*$', interface)
-		if int is None:
-			interface = 'unknown'
-			duplex = 'unknown'
-			speed = 'unknown'
-			return interface, duplex, speed
-		else:
-			int = int.group()
-	else:
-		int = int.group()
+    if int is None:
+        po = re.search(int_po_regex, interface)
+        print(po)
+        if po is None:
+            print(po)
+            interface = 'unknown'
+            duplex = 'unknown'
+            speed = 'unknown'
+            CRC = 'unknown'
+            input_error = 'unknown'
 
-	summary = net_connect.send_command("show int " + str(int))
+            return interface, duplex, speed, CRC, input_error
+        else:
+            po = po.group()
+            print(po)
+            interface = net_connect.send_command("show run int " + str(po))
+            print(interface)
+            print('*' * 50)
+            next_switch = re.search(description_regex, interface)
+            next_switch = next_switch.group()
+            print(next_switch)
+            return next_switch
 
-	for media in summary.splitlines():
-		if 'media' in media:
-			duplex = media.split(',')[0].strip()
-			speed = media.split(',')[1].strip()
+    else:
+        int = int.group()
 
-	for errors in summary.splitlines():
-		if 'CRC' in errors:
-			error = errors.split(',')
-			CRC = error[1]
-			input_error = error[0]
-			CRC = CRC.strip()
-			CRC = CRC[0]
-			input_error = input_error.strip()
-			input_error = input_error[0]
-	net_connect.disconnect()
-	return int, duplex, speed, CRC, input_error
+    summary = net_connect.send_command("show int " + str(int))
+
+    for media in summary.splitlines():
+        if 'media' in media:
+            duplex = media.split(',')[0].strip()
+            speed = media.split(',')[1].strip()
+
+    for errors in summary.splitlines():
+        if 'CRC' in errors:
+            error = errors.split(',')
+            CRC = error[1]
+            input_error = error[0]
+            CRC = CRC.strip()
+            CRC = CRC[0]
+            input_error = input_error.strip()
+            input_error = input_error[0]
+
+    net_connect.disconnect()
+    return int, duplex, speed, CRC, input_error
+
 
 while True:
 
-	switch = GetNextSwitch(MAC)
-	if switch is not None:
-		print("The mac address is connected to " + switch)
-	else:
-		print("Mac address not found!!")
-		break
+    switch = GetNextSwitch(MAC)
+    if switch is not None:
+        print("The mac address is connected to " + switch)
+    else:
+        print("Mac address not found!!")
+        break
 
-	if FindAttachedInterface(switch,MAC) is not None:
-		interface, duplex, speed, CRC, input_error = FindAttachedInterface(switch, MAC)
-		print("Mac address found")
-		print(interface)
-		print("Duplex: " + duplex)
-		print("Speed: " + speed)
-		print("CRC errors: " + CRC)
-		print("Input_errors: " + input_error)
-		break
+    if "ns-s" or "nw-s" in FindAttachedInterface(switch, MAC):
+        print('*' * 50)
+        print("Connecting to another switch")
+        second_switch = FindAttachedInterface(switch, MAC)
+        interface, duplex, speed, CRC, input_error = FindAttachedInterface(second_switch, MAC)
+        print("Mac address found \n")
+        print(second_switch)
+        print(interface)
+        print("Duplex: " + duplex)
+        print("Speed: " + speed)
+        print("CRC errors: " + CRC)
+        print("Input_errors: " + input_error)
+        break
 
-	else:
-		print('Mac address not found')
-		break
 
+    else:
+        print("Mac address found \n")
+        interface, duplex, speed, CRC, input_error = FindAttachedInterface(switch, MAC)
+
+        print(switch)
+        print(interface)
+        print("Duplex: " + duplex)
+        print("Speed: " + speed)
+        print("CRC errors: " + CRC)
+        print("Input_errors: " + input_error)
+        break
 
 input("Press Enter to Exit")
 dist_connect.disconnect()
