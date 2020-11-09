b59e75175da662763cb702cba7b6e69b6e83dc61
vampep <vampep@optonline.net>
2020-11-09 10:09:14 -0500

    finally working well.

--- Nornir/config changes.py
+++ Nornir/config changes.py
@@ -1,106 +1,123 @@
-from netmiko import ConnectHandler
 from nornir import InitNornir
-from nornir.plugins.tasks import commands
 from nornir.plugins.functions.text import print_result
-from nornir.plugins.tasks.networking import netmiko_send_command
 from nornir.plugins.tasks.networking import netmiko_send_config
-from nornir.plugins.tasks.networking import napalm_configure
+from nornir.core.filter import F
 import openpyxl
+from tqdm import tqdm
 
 wb = openpyxl.load_workbook("all devices Spectrum.xlsx")
 ws = wb.active
+hospitals = ["Forest Hills Hospital", "Glen Cove Hospital", "Huntington Hospital", "LHH", "LIJ", "Mather Hospital",
+             "MEETH", "MIT", "NSUH", "NWH", "PBMC", "Phelps Hospital", "Plainview Hospital", "SIUH",
+             "South Oaks Hospital", "Southshore Hospital", "Syosset Hospital", "Valley Stream Hospital", "Westbury", ]
+
+
+def print_hospitals():
+    print('1. Forest Hills Hospital')
+    print('2. Glen Cove Hospital')
+    print('3. Huntington Hospital')
+    print('4. "LHH')
+    print('5. LIJ')
+    print('6. Mather Hospital')
+    print('7. MEETH')
+    print('8. MIT')
+    print('9. NSUH')
+    print('10. NWH')
+    print('11. PBMC')
+    print('12. Phelps Hospital')
+    print('13. Plainview Hospital')
+    print('14. SIUH')
+    print('15. South Oaks Hospital')
+    print('16. Southshore Hospital')
+    print('17. Syosset Hospital')
+    print('18. Valley Stream Hospital')
+    print('19. Westbury')
 
 
 nr = InitNornir(core={"num_workers": 50},
-	inventory={
-		"plugin": "nornir.plugins.inventory.simple.SimpleInventory",
-		"options": {
-			"host_file": "inventory/hosts.yaml",
-			"group_file": "inventory/groups.yaml",
-			"default_file": "inventory/defaults.yaml"
-		}
-	}
-)
+                inventory={
+                    "plugin": "nornir.plugins.inventory.simple.SimpleInventory",
+                    "options": {
+                        "host_file": "inventory/hosts.yaml",
+                        "group_file": "inventory/groups.yaml",
+                        "default_file": "inventory/defaults.yaml"
+                    }
+                }
+                )
 
-def snmp_file(task):
-	file_name = task.host.get('img')
-	result = task.run(task=netmiko_send_config, config_file=file_name)
-	return result
 
 def results_file(results):
-	for host in results:
-		f = open(f"{host}.txt", "w")
-		f.write(results[host].result)
+    for host in results:
+        print(host)
+        if host is not None:
+            f = open(f"{host}.txt", "w")
+            f.write(results[host].result)
+        else:
+            print("None value for some reason")
 
 
 def to_excel(device):
-	for host in device:
-		working = device[host].result
-		for cell in ws['A']:
-			if cell.value is not None:
-				if host in cell.value:
-					row = cell.row
-		if "configure" in working:
-			ws.cell(row=row, column=9, value="Completed")
+    for host in device:
+        for cell in ws['A']:
+            if cell.value is not None:
+                if host in cell.value:
+                    row = cell.row
 
-		else:
-			ws.cell(row=row, column=9, value="Failed")
-
-WB_nexus = nr.filter(site='Westbury').filter(type='Cisco NX OS')
-
-# Below checks all hosts are found
-# print("---"*20)
-# print("NEXUS")
-# print(MIT_nexus.inventory.hosts)
-
-WB_ios = nr.filter(site='Westbury').filter(type='Cisco IOS - SSH Capable')
-
-# Below checks all hosts are found
-# print("---"*20)
-# print("IOS")
-# print(MIT_ios.inventory.hosts)
-
-WB_inter = nr.filter(site='Westbury').filter(type='Internetwork')
-
-# Below checks all hosts are found
-# print("---"*20)
-# print("INTER")
-# print(MIT_inter.inventory.hosts)
-
-nexus = WB_nexus.run(task=netmiko_send_config,
-						config_file='nexus.txt')
-
-# wr_nexus = MIT_nexus.run(task=netmiko_send_command, command_string='wr')
-
-ios = WB_ios.run(task=netmiko_send_config,
-						config_file='ios.txt')
-
-# wr_ios = MIT_ios.run(task=netmiko_send_command, command_string='wr')
-
-inter = WB_inter.run(task=netmiko_send_config,
-						config_file='internetwork.txt')
-
-print_result(nexus)
-print_result(ios)
-print_result(inter)
-
-results_file(nexus)
-to_excel(nexus)
-
-results_file(ios)
-to_excel(ios)
-
-results_file(inter)
-to_excel(inter)
+        if device[host].failed is True:
+            ws.cell(row=row, column=9, value="Failed")
+        else:
+            ws.cell(row=row, column=9, value="Success")
 
 
+def updates(task, progress):
+    file_name = task.host.get('img')
+    result = task.run(task=netmiko_send_config, config_file=file_name, delay_factor=2, max_loops=400)
+    progress.update()
 
+
+print(print_hospitals())
+print("----" * 40)
+while True:
+    count = input('How many sites are you updating? (1-3): ')
+    count = int(count)
+    if count == 1:
+        selection = input("Please choose which hospital to update from the list above by their number: ")
+        selection = hospitals[int(selection) - 1]
+        print('The following will be updated ' + selection)
+        location = nr.filter(F(site=selection))
+        break
+    elif count == 2:
+        selection1 = input("Please choose the first hospital to update from the list above by their number: ")
+        selection1 = hospitals[int(selection1) - 1]
+        selection2 = input("Select the second hospital: ")
+        selection2 = hospitals[int(selection2) - 1]
+        print('The following will be updated ' + selection1 + ' and ' + selection2)
+        location = nr.filter(F(site=selection1) | F(site=selection2))
+        break
+    elif count == 3:
+        selection1 = input("Please choose the first hospital to update from the list above by their number: ")
+        selection1 = hospitals[int(selection1) - 1]
+        selection2 = input("Select the second hospital: ")
+        selection2 = hospitals[int(selection2) - 1]
+        selection3 = input("Select the third hospital: ")
+        selection3 = hospitals[int(selection3) - 1]
+        print('The following will be updated ' + selection1 + ', ' + selection2 + " and " + selection3)
+        location = nr.filter(F(site=selection1) | F(site=selection2) | F(site=selection3))
+        break
+    else:
+        print("Invalid input")
+        continue
+
+#devices = location.filter(F(type="Cisco IOS - SSH Capable") | F(type="Cisco NX OS") | F(type="Telnet"))
+devices = location.filter(F(type="Cisco IOS - SSH Capable"))
+
+
+with tqdm(total=len(devices.inventory.hosts), desc="Updating  ") as progress_bar:
+    complete = devices.run(task=updates, progress=progress_bar)
+
+print_result(complete)
+to_excel(complete)
 wb.save('all devices Spectrum.xlsx')
-
-
-
-
-
-
+results_file(complete)
 
 
--- Nornir/snmp list.py
+++ Nornir/snmp list.py
@@ -1,13 +1,8 @@
-from netmiko import ConnectHandler
 from nornir import InitNornir
-from nornir.plugins.tasks import commands
 from nornir.plugins.functions.text import print_result
-from nornir.plugins.tasks.networking import netmiko_send_command
-from nornir.plugins.tasks.networking import netmiko_send_config
-from nornir.plugins.tasks.networking import napalm_configure
 from nornir.core.filter import F
 import openpyxl
-
+from openpyxl import Workbook
 
 nr = InitNornir(core={"num_workers": 100},
                 inventory={
@@ -19,24 +14,80 @@ nr = InitNornir(core={"num_workers": 100},
                     }
                 }
                 )
-wb = openpyxl.load_workbook("HH-s-r-todo.xlsx")
+wb = Workbook()
+
+file = input("Enter the name for the excel spreadsheet: ")
+file = file + ".xlsx"
 ws = wb.active
+
 ws["A1"] = "Hosts"
 row = 2
-HH_nexus = nr.filter(site='Huntington Hospital').filter(type='Cisco NX OS').inventory.hosts.keys()
-HH_ios = nr.filter(site='Huntington Hospital').filter(type='Cisco IOS - SSH Capable').inventory.hosts.keys()
-HH_inter = nr.filter(site='Huntington Hospital').filter(type='Internetwork').inventory.hosts.keys()
 
-for host in HH_nexus:
-    ws.cell(row=row, column=1, value=host)
+hospitals = ["Forest Hills Hospital", "Glen Cove Hospital", "Huntington Hospital", "LHH", "LIJ", "Mather Hospital",
+             "MEETH", "MIT", "NSUH", "NWH", "PBMC", "Phelps Hospital", "Plainview Hospital", "SIUH",
+             "South Oaks Hospital", "Southshore Hospital", "Syosset Hospital", "Valley Stream Hospital", "Westbury", ]
+
+
+def print_hospitals():
+    print('1. Forest Hills Hospital')
+    print('2. Glen Cove Hospital')
+    print('3. Huntington Hospital')
+    print('4. "LHH')
+    print('5. LIJ')
+    print('6. Mather Hospital')
+    print('7. MEETH')
+    print('8. MIT')
+    print('9. NSUH')
+    print('10. NWH')
+    print('11. PBMC')
+    print('12. Phelps Hospital')
+    print('13. Plainview Hospital')
+    print('14. SIUH')
+    print('15. South Oaks Hospital')
+    print('16. Southshore Hospital')
+    print('17. Syosset Hospital')
+    print('18. Valley Stream Hospital')
+    print('19. Westbury')
+
+
+print(print_hospitals())
+
+while True:
+    count = input('How many sites are you updating? (1-3): ')
+    count = int(count)
+    if count == 1:
+        selection = input("Please choose which hospital to update from the list above by their number: ")
+        selection = hospitals[int(selection) - 1]
+        print('The following will be updated ' + selection)
+        location = nr.filter(F(site=selection))
+        break
+    elif count == 2:
+        selection1 = input("Please choose the first hospital to update from the list above by their number: ")
+        selection1 = hospitals[int(selection1) - 1]
+        selection2 = input("Select the second hospital: ")
+        selection2 = hospitals[int(selection2) - 1]
+        print('The following will be updated ' + selection1 + ' and ' + selection2)
+        location = nr.filter(F(site=selection1) | F(site=selection2))
+        break
+    elif count == 3:
+        selection1 = input("Please choose the first hospital to update from the list above by their number: ")
+        selection1 = hospitals[int(selection1) - 1]
+        selection2 = input("Select the second hospital: ")
+        selection2 = hospitals[int(selection2) - 1]
+        selection3 = input("Select the third hospital: ")
+        selection3 = hospitals[int(selection3) - 1]
+        print('The following will be updated ' + selection1 + ', ' + selection2 + " and " + selection3)
+        location = nr.filter(F(site=selection1) | F(site=selection2) | F(site=selection3))
+        break
+    else:
+        print("Invalid input")
+        continue
+print('--' * 40)
+devices = location.filter(F(type="Cisco IOS - SSH Capable") | F(type="Cisco NX OS") | F(type="Telnet")).inventory.hosts.keys()
+
+for device in devices:
+    print(device)
+    ws.cell(row=row, column=1, value=device)
     row += 1
 
-for host in HH_ios:
-    ws.cell(row=row, column=1, value=host)
-    row += 1
-
-for host in HH_inter:
-    ws.cell(row=row, column=1, value=host)
-    row += 1
-
-wb.save("HH-s-r-todo.xlsx")
+wb.save(file)
