2377c32500aa0ccfd5238a4c5cc13fc142bf1cea
vampep <vampep@optonline.net>
2021-04-08 16:00:21 -0400

    new linux scripts

---
+++ Linux/IP_search.py
@@ -0,0 +1,234 @@
+# create by Chris Paroly
+
+import openpyxl
+from openpyxl import Workbook
+import socket
+from netmiko import ConnectHandler
+from netmiko import NetMikoTimeoutException
+from netmiko import NetMikoAuthenticationException
+import re
+import ipaddress
+import getpass
+import os
+# get the user's home folder
+homedir = os.path.expanduser('~')
+
+mac_regex = re.compile(r'[0-9a-f]{4}\.[0-9a-f]{4}\.[0-9a-f]{4}')
+int_regex = re.compile(r'Fa{1}\S*\d/\S*\d{1,2}|Gi{1}\S*\d/\S*\d|Eth{1}\d/\S*\d{1,2}|Te{1}\S*\d/\S*\d')
+int_po_regex = re.compile(r'Po{1}\d*')
+int_regexes = [int_regex, int_po_regex]
+description_regex = re.compile(r'Description: (.*)', re.MULTILINE)
+access_vlan_regex = re.compile(r'switchport access vlan (\d*)', re.MULTILINE)
+
+wb = Workbook()
+ws = wb.active
+
+ws['A1'] = 'IPs'
+ws['B1'] = 'Switch'
+ws['C1'] = 'Interface'
+ws['D1'] = 'Duplex'
+ws['E1'] = 'Speed'
+ws['F1'] = 'CRC'
+ws['G1'] = 'Input Errors'
+ws['H1'] = 'Output Drops'
+
+user = input("Tacacs username: ")
+pwd = getpass.getpass("Tacacs password")
+
+while True:
+    try:
+        print("Make sure the file is in the Documents folder")
+        file = input("What is the name of the file (include the .xlsx): ")
+        book = openpyxl.load_workbook(f'{homedir}/Documents/{file}')
+
+        sheet = book.active
+        break
+    except ValueError:
+        print("Wrong file path")
+        continue
+
+col = input("What column number are the IPs? ")
+try:
+    distribution = input("What is the distribution switch that will have the ARP table for these? ")
+    distribution = ipaddress.ip_address(distribution)
+except ValueError:
+    distribution = socket.gethostbyname(distribution)
+
+    cisco = {
+        'device_type': 'cisco_ios',
+        'host': distribution,
+        'username': user,
+        'password': pwd
+    }
+    try:
+        dist_connect = ConnectHandler(**cisco)
+    except NetMikoAuthenticationException:
+        print("Username/password incorrect")
+
+list_ips = []
+
+count = 1
+# get list of IPs from the file
+for column in sheet["A"]:
+    list_ips.append(column.value)
+
+list_ips = iter(list_ips)
+next(list_ips)
+ips = list(filter(None, list_ips))
+
+
+def ConnectToDevice(host):
+    # Connects to the devices
+    print("Connecting to " + host)
+    switch = {
+        'device_type': 'cisco_ios',
+        'host': host,
+        'username': user,
+        'password': pwd
+    }
+    try:
+        net_connect = ConnectHandler(**switch)
+    except NetMikoAuthenticationException:
+        raise
+
+    except NetMikoTimeoutException:
+        raise
+
+    return net_connect
+
+
+def GetMac(ip):
+    # get the mac address associated with the IP from the arp table
+    print('*' * 50)
+    print('Getting the MAC ADDRESS')
+    try:
+        arp = dist_connect.send_command("show ip arp " + ip, delay_factor=.2)
+        print(arp)
+        mac = re.search(mac_regex, arp)
+        if mac is None:
+            print('No arp entry found')
+            ws.cell(row=count, column=3, value='NONE')
+            return None
+        else:
+            mac = mac.group()
+            return mac
+    except:
+        pass
+
+
+def GetNextSwitch(mac):
+    # Gets the switch the mac is connected to
+    print('*' * 50)
+    print('Getting next switch')
+    int = dist_connect.send_command("show mac address-table address " + mac, delay_factor=.1)
+    # Checks to see if it is a port-channel or regular interface
+    po = re.search(int_po_regex, int)
+    if po is None:
+        # looks for the interface since it wasn't a port-channel
+        po = re.search(int_regex, int)
+
+        if po is None:
+            return None
+        else:
+            po = po.group()
+    else:
+        po = po.group()
+
+    print(po)
+    # find the next switch by reading the description of the Port-Channel or Interface
+    interface = dist_connect.send_command("show run int " + str(po))
+    print(interface)
+    for description in interface.splitlines():
+        if 'description' in description.lower():
+            next_switch = description.split(' ')
+
+    next_switch = [switch for switch in next_switch if 'ns-s' in switch or 'nw-s' in switch]
+    next_switch = str(next_switch)[1:-1]
+    next_switch = next_switch.strip("'")
+    print(next_switch)
+
+    return next_switch
+
+
+def FindAttachedInterface(switch, mac):
+    # looks for the mac's interface to supply interface information
+    try:
+        net_connect = ConnectToDevice(switch)
+    except NetMikoAuthenticationException:
+        return "Failed to Connect"
+    except NetMikoTimeoutException:
+        return "Failed to Connect"
+    except ValueError:
+        print(ValueError)
+        return "Failed to Connect"
+    interface = net_connect.send_command("show mac address-table address " + mac)
+    print(interface)
+    int = re.search(int_regex, interface)
+    # if the mac isn't found
+    if int is None:
+        int = re.search(r'Po{1}\d*$', interface)
+        if int is None:
+            interface = 'unknown'
+            duplex = 'unknown'
+            speed = 'unknown'
+            CRC = 'unknown'
+            input_error = 'unknown'
+            output_drops = 'unknown'
+            return interface, duplex, speed, CRC, input_error, output_drops
+        else:
+            int = int.group()
+    else:
+        int = int.group()
+
+    interfaces = net_connect.send_command("show int " + str(int), use_genie=True)
+    # supply us with all needed information from the interface
+    for interface in interfaces:
+        bandwidth = interfaces[interface]['bandwidth']
+        duplex = interfaces[interface]['duplex_mode']
+        speed = interfaces[interface]['port_speed']
+        CRC = interfaces[interface]['counters']['in_crc_errors']
+        input_error = interfaces[interface]['counters']['in_errors']
+        output_drops = interfaces[interface]['queues']['total_output_drop']
+
+    net_connect.disconnect()
+    return interface, duplex, speed, CRC, input_error, output_drops
+
+
+print("Connecting to " + str(distribution))
+
+for ip in ips:
+    # runs through the list of IPs from the excel file and starts the process of getting the mac address
+    # and tracking down the interface it is connected to.
+    print(ip)
+    count += 1
+    mac = GetMac(ip)
+    if mac is None:
+        print('No arp entry found')
+        ws.cell(row=count, column=3, value='NONE')
+        continue
+    switch = GetNextSwitch(mac)
+    if switch is None:
+        print('No interface found---GetNetworkSwitch')
+        ws.cell(row=count, column=3, value='NONE')
+        continue
+    print('The host is connected to ' + switch)
+    if FindAttachedInterface(switch, mac) == 'Failed to Connect':
+        continue
+    interface, duplex, speed, CRC, input_error, output_drops = FindAttachedInterface(switch, mac)
+    if interface is None:
+        print('No Interface found----FindAttachedInterface')
+        ws.cell(row=count, column=3, value='NONE')
+        continue
+    ws.cell(row=count, column=1, value=ip)
+    ws.cell(row=count, column=3, value=interface)
+    ws.cell(row=count, column=4, value=duplex)
+    ws.cell(row=count, column=5, value=speed)
+    ws.cell(row=count, column=2, value=switch)
+    ws.cell(row=count, column=6, value=CRC)
+    ws.cell(row=count, column=7, value=input_error)
+    ws.cell(row=count, column=8, value=output_drops)
+
+input("Press enter to exit")
+wb.save(filename=f'{homedir}/Documents/host-interfaces.xlsx')
+dist_connect.disconnect()
+book.close()
---
+++ Linux/switchport-mapping.py
@@ -0,0 +1,175 @@
+# Created by Chris Paroly
+# Northwell Health
+# Manager: Thomas Hynes
+
+from netmiko import ConnectHandler
+from netmiko import NetMikoTimeoutException
+from netmiko import NetMikoAuthenticationException
+from openpyxl import Workbook
+import socket
+import getpass
+import os
+
+homedir = os.path.expanduser('~')
+
+# Opens Excel file
+
+
+
+def get_mac_address():
+    switch_mac = []
+    switch_port = []
+    switch_mac_table = {}
+    print("******" * 50)
+    print(switch_mac_table)
+    # Gets the mac address table from the switch and gives a dictionary with Mac address and the Port
+    print("getting MAC table from " + mapping)
+    print(switch_connect.find_prompt())
+    mac_address = switch_connect.send_command('show mac address-table', use_textfsm=True)
+    for switch_macs in mac_address:
+        if switch_macs['destination_port'].lower().startswith(('v', 'p', 'c')):
+            continue
+        else:
+            switch_mac.append(switch_macs['destination_address'])
+            switch_port.append(switch_macs['destination_port'])
+    switch_mac_table = dict(zip(switch_mac, switch_port))
+    print(switch_mac_table)
+    return switch_mac_table
+
+
+def collect_information(mac_address_table):
+    # get int status to get the interfaces information
+    print(switch_connect.find_prompt())
+    status = switch_connect.send_command('show int status', use_textfsm=True)
+    count = 2
+    for interface in status:
+        try:
+            port = interface['port']
+            desc = interface['name']
+            vlan = interface['vlan']
+            status = interface['status']
+        except TypeError as t:
+            print(t)
+            continue
+        # Adds that information to the excel spreadsheet
+        ws.cell(row=count, column=1, value=str(port))
+        ws.cell(row=count, column=2, value=str(status))
+        ws.cell(row=count, column=3, value=str(desc))
+        ws.cell(row=count, column=4, value=str(vlan))
+
+        for address, mac_port in mac_address_table.items():
+            if "GigabitEthernet" in mac_port:
+                mac_port = mac_port.replace("GigabitEthernet", "Gi")
+            elif "FastEthernet" in mac_port:
+                mac_port = mac_port.replace("FastEthernet", "Fa")
+            else:
+                pass
+            if mac_port == port:
+                # finds the mac address per port
+                print(mac_port, port)
+                ws.cell(row=count, column=5, value=address)
+                for mac_address, ip_address in arp_table.items():
+                    # find the ip address per mac address
+                    if mac_address == address:
+                        ws.cell(row=count, column=6, value=ip_address)
+                        try:
+                            # gets the hostname for each IP address and adds it to the excel, or value is NONE
+                            hostname = socket.gethostbyaddr(ip_address)
+                            ws.cell(row=count, column=7, value=str(hostname[0]))
+                        except Exception:
+                            ws.cell(row=count, column=7, value="NONE")
+
+        # counts for each row
+        count += 1
+
+    # saves the excel to the Documents folder for each switch
+    wb.save(f"{homedir}/Documents/{mapping}-port-mapping.xlsx")
+    wb.close()
+
+
+switches = []
+
+user = input("Tacacs username: ")
+pwd = getpass.getpass("Tacacs password")
+
+dist_switch = input("What is the distribution switch to get the ARP table from?: ")
+while True:
+    switch = input("What switch are you scanning?: ")
+    switches.append(switch)
+    again = input("Is there another switch you are scanning? (Y/N) ")
+    if again.lower() == 'y':
+        continue
+    else:
+        break
+
+cisco = {
+    'device_type': 'cisco_ios',
+    'host': dist_switch,
+    'username': user,
+    'password': pwd
+}
+
+ips = []
+macs = []
+
+try:
+    net_connect = ConnectHandler(**cisco)
+except NetMikoAuthenticationException:
+    print('Wrong Username or Password')
+except NetMikoTimeoutException:
+    print("Connection to Switch failed")
+
+arp = net_connect.send_command("show ip arp ", use_genie=True)
+# Does a show ip arp command and produces all mac address and IP address
+for key in arp:
+    for value in arp[key]:
+        try:
+            for neighbor in arp[key][value]['ipv4']['neighbors']:
+                mac = arp[key][value]['ipv4']['neighbors'][neighbor]['link_layer_address']
+                macs.append(mac)
+
+                ip = arp[key][value]['ipv4']['neighbors'][neighbor]['ip']
+                ips.append(ip)
+        except KeyError:
+            ip = arp[key][value]['ip_address']
+            ips.append(ip)
+
+            mac = arp[key][value]['mac_address']
+            macs.append(mac)
+
+arp_table = dict(zip(macs, ips))
+
+for mapping in switches:
+    wb = Workbook()
+    ws = wb.active
+    ws['A1'] = 'Interface'
+    ws['B1'] = 'Status'
+    ws['C1'] = 'Description'
+    ws['D1'] = 'Vlan'
+    ws['E1'] = 'MAC'
+    ws['F1'] = 'IP Address'
+    ws['G1'] = 'Hostname'
+
+    # loops over each switch in the list given
+    switch = {
+        'device_type': 'cisco_ios',
+        'host': mapping,
+        'username': user,
+        'password': pwd
+    }
+
+    try:
+        print("connecting to " + mapping)
+        switch_connect = ConnectHandler(**switch)
+    except NetMikoAuthenticationException:
+        print('Wrong Username or Password')
+        break
+    except NetMikoTimeoutException:
+        print("Connection to Switch failed")
+        continue
+    print("sending to get_mac_address")
+    mac_table = get_mac_address()
+    print("sending to collect_information")
+    collect_information(mac_table)
+
+print("Check the Documents folder for the excel spreadsheets")
